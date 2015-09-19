adb3c636f8f9bf494a82f41365c69e257d1ec88c
LuddeW <ludwig.wiktorsson@gmail.com>
2015-09-19 19:11:28 +0200

    First Commit

---
+++ WhackAMole/Game1.cs
@@ -0,0 +1,299 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+
+namespace WhackAMole
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D Background;
+        Texture2D Front;
+        Texture2D RabbitSprite;
+        Texture2D ClockSprite;
+        Texture2D EndScreen;
+        Texture2D ClockSheet;
+        Texture2D RabbitDead;
+        RabbitClass[] Rabbits = new RabbitClass[3];
+        SpriteFont Font;
+        Vector2 MousePos;
+        public Vector2 TempPos;
+        public float X0 = 43;
+        public float X1 = 450 / 2 - 65;
+        public float X2 = 275;
+        public float StopY;
+        public int Velocity;
+        public int NewVelocity;
+        bool PrevMState;
+        int Score = 0;
+        float PlayTime;
+        int TimePlayed;
+        int iX;
+        int iY;
+        int xPos;
+        int yPos;
+        public bool Dead = false;
+        
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 450;
+            graphics.PreferredBackBufferHeight = 450;
+            graphics.ApplyChanges();
+            IsMouseVisible = true;
+            
+
+            Content.RootDirectory = "Content";
+        }
+     
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            LoadPictures();
+            LoadRabbits();
+            LoadFonts();
+            
+            
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            
+            // TODO: Add your update logic here
+              PlayTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
+              TimePlayed = (int)PlayTime;
+              
+              Rabbits[0].Movement();
+              Rabbits[1].Movement();
+              Rabbits[2].Movement();
+
+              ScoreCount();
+            
+              base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+           
+            DrawPictures();
+            
+            DrawFonts();
+
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+        protected void LoadPictures()
+        {
+            Background = Content.Load<Texture2D>(@"rabbit_background");
+            Front = Content.Load<Texture2D>(@"rabbit_front");
+            RabbitSprite = Content.Load<Texture2D>(@"rabbit_ok");
+            ClockSprite = Content.Load <Texture2D>(@"clock_0");
+            EndScreen = Content.Load<Texture2D>(@"end_screen");
+            ClockSheet = Content.Load<Texture2D>(@"Clock_sheet");
+            RabbitDead = Content.Load<Texture2D>(@"rabbit_dead");
+            
+        }
+        protected void DrawPictures()
+        {
+
+            spriteBatch.Draw(Background, new Rectangle(0, 0, Background.Width, Background.Height), Color.White);
+            Vector2 CenterClock = new Vector2(Window.ClientBounds.Width / 2 - ClockSprite.Width / 2, 10);
+            spriteBatch.Draw(ClockSprite, CenterClock, Color.White);
+            SheetPos();
+            for (int i = 0; i < 3; i++)
+            {
+                if (Rabbits[i].Dead == false)
+                {
+
+                   spriteBatch.Draw(RabbitSprite, Rabbits[i].RabbitPos, Color.White);
+                }
+                else
+                {
+                    spriteBatch.Draw(RabbitDead, Rabbits[i].RabbitPos, Color.White);
+                }
+            }
+            
+            
+            spriteBatch.Draw(Front, new Vector2(0, Window.ClientBounds.Height - Front.Height), Color.White);
+            End();
+
+        }
+        protected void LoadRabbits()
+        {
+           
+            TempPos.Y = Window.ClientBounds.Height ;
+            Random Rnd = new Random();
+            float[] XPositions = new float[3];
+            XPositions[0] = X0;
+            XPositions[1] = X1;
+            XPositions[2] = X2;
+            for (int i = 0; i < 3; i++)
+            {
+
+                Velocity = Rnd.Next(-8, -1);
+
+                TempPos.X = XPositions[i];
+                Rabbits[i] = new RabbitClass(TempPos, StopY, Front.Height, RabbitSprite.Width, RabbitSprite.Height, Velocity, Dead);
+            }
+            
+       }
+
+        protected void LoadFonts()
+        {
+            Font = Content.Load<SpriteFont>(@"NewSpriteFont");
+        }
+
+        protected void DrawFonts()
+        {
+            string score = "Score:" + Score;
+            if (TimePlayed >= 60)
+            {
+                
+                spriteBatch.DrawString(Font, score, new Vector2(Window.ClientBounds.Width/2 , Window.ClientBounds.Height/2 + 25) , Color.White);
+            }
+            else
+            {
+                Vector2 scorelen = Font.MeasureString(score);
+                spriteBatch.DrawString(Font, score, new Vector2(Window.ClientBounds.Width / 2 - scorelen.X / 2, Window.ClientBounds.Height - (Front.Height - 50)), Color.White);
+            }
+            
+        }
+
+        protected void ScoreCount()
+        {
+            MousePos = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
+            
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                if (!PrevMState)
+                {
+                    RabbitHit();                    
+                }   
+                
+                }
+            PreviousMouseState();
+
+        }
+      
+        protected void PreviousMouseState()
+        {
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                PrevMState = true;
+            }
+            else
+            {
+                PrevMState = false;
+            }
+        }
+
+        protected void RabbitHit()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (Rabbits[i].Dead == false)
+                {
+
+
+                    if (Rabbits[i].Hitbox.Contains(MousePos) && MousePos.Y < Window.ClientBounds.Height - Front.Height)
+                    {
+                        Score = Score + 10;
+                        Rabbits[i].Dead = true;
+
+                    }
+                }
+            }
+        }
+
+        protected void End()
+        {
+            if (TimePlayed >= 60)
+            {
+                spriteBatch.Draw(EndScreen, new Rectangle(0, 0, Background.Width, Background.Height), Color.White);
+                
+                for (int i = 0; i < 3; i++)
+                {
+                    Rabbits[i].RabbitPos.Y = 450;
+                    Rabbits[i].Velocity = 0;
+                }
+                
+            }
+        }
+      
+        protected void SheetPos()
+        {
+            
+            iY = TimePlayed / 10;
+            iX = TimePlayed % 10;
+            xPos = 122 * iX;
+            yPos = 122 * iY;
+
+            spriteBatch.Draw(ClockSheet, new Rectangle(Window.ClientBounds.Width / 2 - 122 / 2, 12, 122, 122),
+            new Rectangle(xPos, yPos, 122, 122), Color.White);
+            
+            
+
+        }  
+        protected void Restart()
+        {
+
+        }
+    }
+
+
+     
+}
---
+++ WhackAMole/RabbitClass.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using System.Threading;
+
+namespace WhackAMole
+{
+    class RabbitClass
+    {
+        public Vector2 RabbitPos;
+        public float StopY;
+        public int FrontHeight;
+        public int RabbitHeight;
+        public int RabbitWidth;
+        public int Velocity;
+        public Rectangle Hitbox;
+        public bool Dead;
+
+
+
+
+        public RabbitClass(Vector2 RabbitPos, float StopY, int FrontHeight, int RabbitWidth, int RabbitHeight, int Velocity, bool Dead)
+        {
+            this.RabbitPos = RabbitPos;
+            this.StopY = StopY;
+            this.FrontHeight = FrontHeight;
+            this.RabbitHeight = RabbitHeight;
+            this.RabbitWidth = RabbitWidth;
+            this.Velocity = Velocity;
+            this.Dead = Dead;
+            Hitbox = new Rectangle((int)RabbitPos.X, (int)RabbitPos.Y, RabbitWidth, RabbitHeight);
+
+        }
+        public void Movement()
+        {
+            
+            Random NewRnd = new Random();
+            StopY = FrontHeight + 0.25f * RabbitHeight;
+            RabbitPos.Y = RabbitPos.Y + Velocity;
+            Hitbox.Y = (int)RabbitPos.Y;
+            if (Dead)
+            {
+                if (Velocity < 0)
+                {
+                    Velocity = -2;
+                }
+                else
+                {
+                    Velocity = 2;
+                }
+            }
+            if (RabbitPos.Y <= StopY )
+            {
+               
+                    Velocity = 2;
+                
+                
+            }
+            
+            else if (RabbitPos.Y >= 450)
+            {
+                
+                Velocity = -4;
+                if (Dead)
+                {
+                    Dead = false;
+                }
+
+            }
+           
+        }
+
+        
+    }
+
+}
